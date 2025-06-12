7b827462777c1d9dd6254469f73502774166ba5a
Niharika Jha <130960755+Niharikajha01@users.noreply.github.com>
2025-06-12 19:19:15 +0530

    Add files via upload

---
+++ backend/model/PositionsModel.js
@@ -0,0 +1,4 @@
+const{model}=require("mongoose");
+const{PositionsSchema}=require('../schemas/PositionsSchema');
+const PositionsModel =new model("postion", PositionsSchema);
+module.exports={PositionsModel};
